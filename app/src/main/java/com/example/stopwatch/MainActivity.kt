116d1f3ba18476de7538fc822f3e47ff4a42a7f2
LeafAsked <evanan0512@gmail.com>
2023-09-06 09:44:34 -0700

    testing out how to make a commit

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.TabStopSpan
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    
+
+    lateinit var start: Button
+    lateinit var reset: Button
+
     // public static final String PI = 3.14     declaring a class-wide constant in java
     // in kotlin, we use a companion object
     companion object {
@@ -25,5 +30,30 @@ class MainActivity : AppCompatActivity() {
     // function that exists in the superclass)
     override fun onStart() {
         super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun widgets() {
+        start = findViewById(R.id.button_main_start)
+        reset = findViewById(R.id.button_main_reset)
     }
 }
